2d362e1381fe688e6a9595d206c6640f42a5f043
Arun Badapure <arunbadapure00000>
2023-04-01 08:05:47 +0530

    add changes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// Boots up the app
 function init() {
-  console.log("website loaded");
+  console.log("WebPage Loaded");
 }
+
+// mobile header ui
+const mobileIcon = document.querySelector(".mobile-icon");
+const header = document.querySelector(".header");
+mobileIcon.addEventListener("click", () => header.classList.toggle("active"));
 
 window.addEventListener("load", function () {
   init();
