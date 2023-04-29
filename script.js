ebe2585e9b164fdd380c99c262cfe69f442c0153
Arun Badapure <arunbadapure00000>
2023-04-29 10:12:43 +0530

    add about section

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const mobileIcon = document.querySelector(".mobile-icon");
 const header = document.querySelector(".header");
 mobileIcon.addEventListener("click", () => header.classList.toggle("active"));
 
+// about section accordion ui
+const accordion = document.getElementsByClassName("content-box");
+for (let i = 0; i <= accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
+
 window.addEventListener("load", function () {
   init();
 });
